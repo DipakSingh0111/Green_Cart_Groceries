2c613de7f4a058508a7a00735cce58dd8b593b33
Dipak Singh <dipaksingh3323@gmail.com>
2025-05-14 19:47:40 +0530

    Cart Page Done.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from './components/Login';
 import AllProducts from './pages/AllProducts';
 import ProductCategory from './pages/ProductCategory';
 import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
 function App() {
   const isSellerPath = useLocation().pathname.includes("seller");
   const { showUserLogin } = useContext(AppContext)
@@ -26,6 +27,7 @@ function App() {
           <Route path='/products' element={<AllProducts />} />
           <Route path='/products/:category' element={<ProductCategory />} />
           <Route path='/products/:category/:id' element={<ProductDetails />} />
+          <Route path='/cart' element={<Cart />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
