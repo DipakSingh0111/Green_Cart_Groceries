80df02e9d11ccdc83c6863de56eacd86a68b0f75
Dipak Singh <dipaksingh3323@gmail.com>
2025-05-15 23:33:29 +0530

    Address Page is Done

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import AllProducts from './pages/AllProducts';
 import ProductCategory from './pages/ProductCategory';
 import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
+import AddAddress from './pages/AddAddress';
 function App() {
   const isSellerPath = useLocation().pathname.includes("seller");
   const { showUserLogin } = useContext(AppContext)
@@ -28,6 +29,7 @@ function App() {
           <Route path='/products/:category' element={<ProductCategory />} />
           <Route path='/products/:category/:id' element={<ProductDetails />} />
           <Route path='/cart' element={<Cart />} />
+          <Route path='/add-address' element={<AddAddress />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
